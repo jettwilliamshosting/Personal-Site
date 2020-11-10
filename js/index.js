10c68e380a07ab750e779d2cc6c4baa9b65bc905
Work Account <WorkAccount@Harrys-MacBook-Pro.local>
2020-11-10 13:54:02 -0600

    inner height js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,10 @@
-window.addEventListener('orientationchange', () => {
-  window.scroll(0, 100);
+window.addEventListener('orientationchange', function() {
+    // After orientationchange, add a one-time resize event
+    var afterOrientationChange = function() {
+        // YOUR POST-ORIENTATION CODE HERE
+        document.body.style.height = window.innerHeight;
+        // Remove the resize event listener after it has executed
+        window.removeEventListener('resize', afterOrientationChange);
+    };
+    window.addEventListener('resize', afterOrientationChange);
 });
