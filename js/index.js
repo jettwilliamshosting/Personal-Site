7a790861a30f0a7143f44960288fd708b356e6ec
Work Account <WorkAccount@Harrys-MacBook-Pro.local>
2020-11-10 14:34:49 -0600

    remove js and jquery from site

--- js/index.js
+++ js/index.js
@@ -1,10 +1,4 @@
-window.addEventListener('orientationchange', function() {
-    // After orientationchange, add a one-time resize event
-    var afterOrientationChange = function() {
-        // YOUR POST-ORIENTATION CODE HERE
-        document.body.style.height = window.innerHeight;
-        // Remove the resize event listener after it has executed
-        window.removeEventListener('resize', afterOrientationChange);
-    };
-    window.addEventListener('resize', afterOrientationChange);
-});
+$(document).ready(function() {
+
+
+})
