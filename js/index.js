ae69097d466e822e635022655eabef4d34052244
Work Account <WorkAccount@Harrys-MacBook-Pro.local>
2020-11-10 13:38:07 -0600

    removed old js

--- js/index.js
+++ js/index.js
@@ -1,19 +1,3 @@
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
-
 window.addEventListener('orientationchange', () => {
   window.scroll(0, 100);
-});
-
-$(window).on('touchmove', function () {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
